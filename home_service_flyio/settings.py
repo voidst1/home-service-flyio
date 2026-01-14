902979aefb6317fd6d8ffe27d24e951e5b8e9ff4
JS <dev@voidst.one>
2026-01-14 23:56:42 +0000

    Add volume for flyio

--- home_service_flyio/settings.py
+++ home_service_flyio/settings.py
@@ -77,7 +77,8 @@ WSGI_APPLICATION = "home_service_flyio.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        #"NAME": BASE_DIR / "db.sqlite3",
+        'NAME': '/data/db.sqlite3',  # for flyio volume
     }
 }
 
