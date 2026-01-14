1b7c5aad27967404e14029ed1463195ca4bc67b6
JS <dev@voidst.one>
2026-01-14 02:20:19 +0000

    Update ALLOWED_HOST

--- home_service_flyio/settings.py
+++ home_service_flyio/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-*%ix(szg#iyiaq^^nr05gq9v6ckpv4ehao^(_fg%3t8m*e@+#i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split()
 
 
 # Application definition
