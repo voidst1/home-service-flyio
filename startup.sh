45cefe9c01d49122aa3388b2af93e7c765063829
JS <dev@voidst.one>
2026-01-27 09:16:27 +0000

    Update automigration script

---
+++ startup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+python manage.py migrate
+sqlite3 /data/db.sqlite3 'PRAGMA journal_mode=WAL;'
+sqlite3 /data/db.sqlite3 'PRAGMA synchronous=1;'
+#gunicorn --bind :8000 --workers 2 yourproject.wsgi
+python manage.py runserver 0.0.0.0:8000
